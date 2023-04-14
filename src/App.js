8766cf7cebc51fc0143d194587600047c942e4e4
“HectorDevelops” <HectorDevelops@gmail.com>
2023-04-14 11:37:21 -0700

    Practice Pokemon API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
 function App() {
+  // Destructuring to update our data
+  // Setting up useState as an empty array as we will be retrieving an array of pokemon
+  const [pokemons, setPokemons] = useState([]);
+
+  const fetchPokemons = () => {
+    // Fetching a total of 807 pokemons
+    fetch("https://pokeapi.co/api/v2/pokemon/?limit=807")
+      .then(response => { return response.json() })
+      .then(response => {
+        setPokemons(response.results);
+        // console.log(response);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Creating the Button to be able to populate list of Pokemon */}
+      <button onClick={fetchPokemons}> Fetch Pokemon </button>
+
+      <ul>
+        {
+          // Creating a map function to iterate over the returned array list and return only name
+          pokemons.map((item, i) => {return <li key={pokemons.id}>{item.name}</li>})
+        }
+      </ul>
     </div>
   );
 }
